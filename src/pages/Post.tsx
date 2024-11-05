2f446c95b6d7ec39c93e808ff87b0fd8d07bf5c6
kader009 <kadermollarasel000@gmail.com>
2024-11-05 23:48:27 +0600

    post type add

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,5 +1,12 @@
 import { useAllPostQuery } from '../redux/api/PostApi';
 
+interface Posts {
+  body: string;
+  id: number;
+  title: string;
+  userId: string;
+}
+
 const ApiPost = () => {
   const { data, isLoading } = useAllPostQuery('');
   console.log(data);
@@ -12,9 +19,9 @@ const ApiPost = () => {
     );
   }
   return (
-    <div className="flex justify-center items-center lg:flex-row min-h-screen">
+    <div className="text-center">
       <h1>All user post here</h1>
-      {data.map((post) => (
+      {data.map((post: Posts) => (
         <div key={post.id}>
           <span>{post.title}</span>
         </div>
