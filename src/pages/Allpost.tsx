8855b58e9e09600cb0b89fa9cadd738ba19605e0
kader009 <kadermollarasel000@gmail.com>
2024-11-07 12:46:39 +0600

    center all post

--- src/pages/Allpost.tsx
+++ src/pages/Allpost.tsx
@@ -16,15 +16,16 @@ const Allpost = () => {
     );
   }
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1>All post here</h1>
-
-      <div className="mt-6">
-        {data.map((post: Posts) => (
-          <div key={post._id}>
-            <h1>{post.text}</h1>
-          </div>
-        ))}
+    <div>
+      <h1 className="text-center capitalize">All post here</h1>
+      <div className="my-8">
+        <div className="space-y-3 flex gap-3 justify-center items-center">
+          {data.map((post: Posts) => (
+            <div key={post._id} className="flex justify-center">
+              <h1 className="bg-black p-2 text-white rounded">{post.text}</h1>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
