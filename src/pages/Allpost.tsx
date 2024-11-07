002bac282711713279b3a39d35c3b5e14edb4b88
kader009 <kadermollarasel000@gmail.com>
2024-11-07 12:01:47 +0600

    show loader

--- src/pages/Allpost.tsx
+++ src/pages/Allpost.tsx
@@ -1,9 +1,16 @@
 import { useGetAllpostQuery } from '../redux/authentication/authApi';
 
 const Allpost = () => {
-  const { data } = useGetAllpostQuery('');
+  const { data, isLoading } = useGetAllpostQuery('');
   console.log(data);
-  return <div className='flex justify-center items-center min-h-screen'><h1>All post here</h1></div>;
+
+  if(isLoading){
+    return <div className='flex justify-center items-center min-h-screen text-red-600'>Loading...</div>
+  }
+  return <div className='flex justify-center items-center min-h-screen'>
+    <h1>All post here</h1>
+    {data.length}
+  </div>;
 };
 
 export default Allpost;
