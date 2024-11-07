74f25dd4befc3b80a63df23c3effeb29ecb48180
kader009 <kadermollarasel000@gmail.com>
2024-11-07 12:07:25 +0600

    error solve using type added

--- src/pages/Allpost.tsx
+++ src/pages/Allpost.tsx
@@ -1,16 +1,33 @@
 import { useGetAllpostQuery } from '../redux/authentication/authApi';
 
+interface Posts {
+  _id: string;
+  text: string;
+}
+
 const Allpost = () => {
   const { data, isLoading } = useGetAllpostQuery('');
-  console.log(data);
 
-  if(isLoading){
-    return <div className='flex justify-center items-center min-h-screen text-red-600'>Loading...</div>
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-red-600">
+        Loading...
+      </div>
+    );
   }
-  return <div className='flex justify-center items-center min-h-screen'>
-    <h1>All post here</h1>
-    {data.length}
-  </div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1>All post here</h1>
+
+      <div className="mt-6">
+        {data.map((post: Posts) => (
+          <div key={post._id}>
+            <h1>{post.text}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Allpost;
